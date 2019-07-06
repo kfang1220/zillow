f7b1fb031dc0ab495869f6de2d985035e3cd9583
kevinfang <fangk1220@gmail.com>
2019-07-06 00:34:48 -0700

    Merging scss files into one css file, also moved css/html/bundle into dist

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import _ from 'lodash';
-import '../assets/style.css';
+import '../assets/style.scss';
+import '../test/style.scss';
 // import Icon from '../components/icon.png';
 // import Data from '../components/data.xml';
 
 
 function component() {
     const element = document.createElement('div');
+    const element2 = document.createElement('div');
 
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello')
+    //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('mod1')
+
+    element.innerHTML = _.join(["I am testing css joining"], ' ')
+    element.classList.add('mod2')
+
+
 
     // const myIcon = new Image();
     // myIcon.src = Icon;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const uuid = require('uuid/v1');
 const globalBundleID = uuid();
 
@@ -8,23 +10,66 @@ module.exports = {
     resolve: { extensions: ['.js', '.jsx', '.json', '.es6'] },
     entry: './src/index.js',
     output: {
-        filename: 'bundle' + globalBundleID + '.js',
+        filename: './dist/bundle__' + globalBundleID + '.js',
         path: path.resolve(__dirname)
     },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Zillow Photo Gallery',
             filename: './dist/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: "./dist/style__" + globalBundleID + ".css"
         })
     ],
     module: {
-        rules: [{
-                test: /\.css$/,
+        rules: [
+            // {
+            //     test: /\.css$/,
+            //     use: [
+            //         "style-loader", // creates style nodes from JS strings
+            //         "css-loader", // translates CSS into CommonJS
+            //         "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            //     ]
+            // },
+            {
+                test: /\.(sa|sc|c)ss$/,
                 use: [
-                    "style-loader", // creates style nodes from JS strings
-                    "css-loader", // translates CSS into CommonJS
-                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
-                ]
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 2,
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                options: { includePaths: [path.resolve(__dirname)] },
+                                sourceMap: true
+                            }
+                        }
+
+
+                        // MiniCssExtractPlugin.loader,
+                        // "css-loader",
+                        // {
+                        //     loader: "sass-loader",
+                        //     options: { includePaths: [path.resolve(__dirname)] }
+
+                        // }
+                    ]
+                    // Want to do something like this to generate a second CSS theme bundle
+                    // use: [
+                    //     MiniCssExtractPlugin.loader,
+                    //     "css-loader",
+                    //     {
+                    //         loader: "sass-loader",
+                    //         options: {  includePaths: [path.resolve(__home, "src/dark-theme"),]}
+                    //
+                    //     }
+                    // ]
             },
             {
                 test: /\.m?js$/,
@@ -36,6 +81,7 @@ module.exports = {
                     }
                 }
             },
+
             // {
             //     test: /\.css$/,
             //     use: [
